c53ca32e86322e6ab9cedd75d8780dcf685ccbc9
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2022-06-07 17:38:38 +0300

    new hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,10 +9,73 @@ if (process.env.REPORT_COVERAGE) {
   require('solidity-coverage');
 }
 
+require('dotenv').config()
+// require('@eth-optimism/plugins/hardhat/compiler')
+// require('@eth-optimism/plugins/hardhat/ethers')
+
+require("@nomiclabs/hardhat-etherscan");
+require( "hardhat-abi-exporter")
+require("@nomiclabs/hardhat-solhint");
+require( "hardhat-deploy")
+require("hardhat-deploy-ethers")
+
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  abiExporter: {
+    path: "./abi",
+    clear: false,
+    flat: true,
+    // only: [],
+    // except: []
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    dev: {
+      // Default to 1
+      default: 1,
+      // dev address mainnet
+      // 1: "",
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      },
+      initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
+    },
+    testnet: {
+      url: process.env.NODE_URL,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      gasPrice: 120 * 1000000000,
+      chainId: 1,
+    },
+    rinkeby: {
+      url: process.env.L1_NODE_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org",
+      chainId: 56,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  paths: {
+    artifacts: "artifacts",
+    cache: "cache",
+    deploy: "deploy",
+    deployments: "deployments",
+    imports: "imports",
+    sources: "contracts",
+    tests: "test",
+  },
   solidity: {
     version: '0.8.14',
     settings: {
